8c36b55faf3b1e7b2e3c4b3c1a6758d1e3ef42b8
Nicolas Cieters <etemp@free.fr>
2020-04-08 23:47:40 +0200

    Petit test serveur client

--- ComServeurClient/Client.py
+++ ComServeurClient/Client.py
@@ -1,1 +1,13 @@
+import	socket
+
+HOST = '192.168.139.1'
+PORT = 8000
+
+connexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+	connexion.connect((Host, PORT))
+except socket.error:
+	printf("erre")
+	exit(1)
 
--- ComServeurClient/Serveur.py
+++ ComServeurClient/Serveur.py
@@ -1,1 +1,28 @@
 
+import	socket
+import time
+
+HOST = '192.168.139.1'
+PORT = 8000
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+try:
+	server.bind((HOST, PORT))
+except socket.error:
+	print("error")
+	exit(1)
+
+print('Serveur en attente')
+	
+while True:
+	server.listen(2)
+	connexion, address = server.accept()
+	print('nouvelle connexion')
+
+	connexion.send('connexion etablie au serveur '.encode('utf-8'))
+	time.sleep(5)
+	print('envoie quit')
+	connexion.send('QUIT'.encode('utf-8'))
+	break
